52adb6cf22347e8391500f1599f9f6a65a1aa17e
fabio21777 <fabiosouza21777@gmail.com>
2021-07-01 16:35:37 -0300

    H2 database, test profile, JPA

--- src/main/java/com/fsm/entities/User.java
+++ src/main/java/com/fsm/entities/User.java
@@ -1,8 +1,20 @@
 package com.fsm.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
