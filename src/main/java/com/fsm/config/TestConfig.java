eba8fadfa8ef9dda92752dc0558a28156f039834
fabio21777 <fabiosouza21777@gmail.com>
2021-07-02 17:28:35 -0300

    Category

--- src/main/java/com/fsm/config/TestConfig.java
+++ src/main/java/com/fsm/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.fsm.entities.Category;
 import com.fsm.entities.Order;
 import com.fsm.entities.User;
 import com.fsm.entities.enun.OrderStatus;
+import com.fsm.repositories.CategoryRepository;
 import com.fsm.repositories.OrderRepository;
 import com.fsm.repositories.UserRepository;
 
@@ -22,8 +24,19 @@ public class TestConfig implements CommandLineRunner  {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
---
+++ src/main/java/com/fsm/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.fsm.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fsm.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+}
