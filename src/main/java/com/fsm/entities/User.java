75fc16d1eb4103972a37c49b3b9a4ade8bee333a
fabio21777 <fabiosouza21777@gmail.com>
2021-07-02 15:21:15 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/fsm/entities/User.java
+++ src/main/java/com/fsm/entities/User.java
@@ -1,15 +1,20 @@
 package com.fsm.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -20,6 +25,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	
 	public User() {
 		// TODO Auto-generated constructor stub
@@ -73,6 +82,9 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(email, id, name, password, phone);
