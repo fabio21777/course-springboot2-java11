9690957ebf856c4fc27d5bba60b11eb1cd009706
fabio21777 <fabiosouza21777@gmail.com>
2021-07-06 17:25:10 -0300

    Subtotal & Total methods

--- src/main/java/com/fsm/entities/OrderItem.java
+++ src/main/java/com/fsm/entities/OrderItem.java
@@ -63,6 +63,9 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
